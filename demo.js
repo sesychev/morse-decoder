2960b33e6b6c666dba205973ee66c9d0157f331b
sesychev <sergeysychou@gmail.com>
2022-07-16 23:04:52 +0300

    feat: finish morse-decoder

--- demo.js
+++ demo.js
@@ -1,3 +1,20 @@
+/*function reverse (n) {
+	const str = Math.abs(n).toString();
+	
+	let res = ''
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		console.log(`${i} ${str[i]}`);
+		res += str[i];
+	}
+
+	return +res;
+}
+
+
+console.log(reverse(250));
+*/
+
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -37,22 +54,28 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-// write your solution here
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+
+function decode(e) {
+    // write your solution here
+	const dot = { '.' : '10' };
+	const dash = { '-' : '11' };
+
 	const length = 10;
 	const alphabet = 2;
 	const space = '**********';
 	let str = '';
 	let array = [];
 	
-	for (let index = 0, j = length; index < expr.length; index += length, j += length) {
-		const element = expr.slice(index, j);
+	for (let index = 0, j = length; index < e.length; index += length, j += length) {
+		const element = e.slice(index, j);
 		if (element === space) {
 			array.push (' ');
 		}
 		else {
 			array.push (letter(element));
 		}
+		//console.log(letter (element));
 	}
 
 	function letter (word) {
@@ -65,8 +88,8 @@ function decode(expr) {
 				encodedLetter += '-';
 			}
 		}
-
-        return encodedLetter;
+		//console.log(encodedLetter)
+		return encodedLetter;
 	}
 
 	for (const item of array) {
@@ -78,9 +101,11 @@ function decode(expr) {
 		}
 	}
 
+	console.log(str);
+	console.log(array);
+
 	return str;
 }
 
-module.exports = {
-    decode
-}
+//console.log(expr.slice(0, 10));
+decode(expr);
